cb84cbb84ea5dc3a7ce8ac85a789b58c52119557
Alexander Esteban Zapata <AlexanderEstebanZapata1994@users.noreply.github.com>
2026-06-12 14:22:34 -0500

    feat: initialize MCP server with fetch-weather tool integration with open-meteo API's

--- 01-start/main.ts
+++ 01-start/main.ts
@@ -1,4 +1,5 @@
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
+import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio';
 import { z } from 'zod';
 
 const server = new McpServer({
@@ -8,6 +9,7 @@ const server = new McpServer({
 
 
 //Defining tools
+// Tools allow LLM do actions throught the server
 
 server.tool(
     'fetch-weather',
@@ -16,11 +18,36 @@ server.tool(
         city: z.string().describe('City name')
     },
     async ({ city }) => {
+
+        const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json`)
+        const data = await response.json()
+
+        if (!data.results?.length) {
+            return {
+                content: [
+                    {
+                        type: 'text',
+                        text: `No weather data found for ${city}`
+                    }
+                ]
+            }
+        }
+
+        const { latitude, longitude } = data.results[0];
+
+        const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current=temperature_2m,rain,apparent_temperature,is_day,precipitation&forecast_days=1`)
+        const weatherData = await weatherResponse.json()
+
         return {
             content: [
-                { type: 'text', text: `The weather in ${city} is sunny` }
+                {
+                    type: 'text',
+                    text: JSON.stringify(weatherData, null, 2)
+                }
             ]
         }
     }
 )
 
+const transport = new StdioServerTransport()
+await server.connect(transport)
